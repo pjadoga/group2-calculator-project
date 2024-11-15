b15cc83cb1f40342137346637c6c31946070b03f
Justin Adoga <pj4just@gmail.com>
2024-11-15 08:29:41 +0100

    Basic Calculator and more from Justin

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
+//NUMERICAL CALCULATOR FUNCTIONS
+// Function that display value 
+function dis(val) { 
+    document.getElementById("result").value += val 
+} 
 
+function myFunction(event) { 
+    if (event.key == '0' || event.key == '1' 
+        || event.key == '2' || event.key == '3' 
+        || event.key == '4' || event.key == '5' 
+        || event.key == '6' || event.key == '7' 
+        || event.key == '8' || event.key == '9' 
+        || event.key == '+' || event.key == '-' 
+        || event.key == '*' || event.key == '/') 
+        document.getElementById("result").value += event.key; 
+} 
+
+var cal = document.getElementById("calcu"); 
+cal.onkeyup = function (event) { 
+    if (event.keyCode === 13) { 
+        console.log("Enter"); 
+        let x = document.getElementById("result").value 
+        console.log(x); 
+        solve(); 
+    } 
+} 
+
+// Function that evaluates the digit and return result 
+function solve() { 
+    let x = document.getElementById("result").value 
+    let y = math.evaluate(x) 
+    document.getElementById("result").value = y 
+} 
+
+// Function that clear the display 
+function clr() { 
+    document.getElementById("result").value = "" 
+} 
+
+
+
+// SI CALCULATOR FUNCTIONS
 function calculateInterest() {
     const principal = parseFloat(document.getElementById('principal').value);
     const interestRate = parseFloat(document.getElementById('interest-rate').value);
-    const days = parseFloat(document.getElementById('days').value);
+    const days= parseFloat(document.getElementById('days').value);
     const months = parseFloat(document.getElementById('months').value);
+    
+     console.log(days);
+     console.log(months);
+    
     const years = parseFloat(document.getElementById('years').value);
     const currency =document.getElementById('currency').value;
 
